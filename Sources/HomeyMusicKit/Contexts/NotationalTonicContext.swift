87e280e18d62ebf28d895b75e989afa120d1fdc7
Brian Mulloy <brian@landlessness.net>
2025-03-11 19:11:21 -0400

    moved help and tonic picker tracker to the notation tonic context

--- Sources/HomeyMusicKit/Contexts/NotationalTonicContext.swift
+++ Sources/HomeyMusicKit/Contexts/NotationalTonicContext.swift
@@ -1,5 +1,10 @@
+import SwiftUI
+
 public class NotationalTonicContext: NotationalContext, @unchecked Sendable {
-    
+
+    @Published public var showHelp: Bool = false
+    @Published public var showTonicPicker: Bool = false
+
     /// Override default note labels to have `.letter` true for the tonic picker.
     public override class func defaultNoteLabels() -> [InstrumentType: [NoteLabelChoice: Bool]] {
         var defaults = super.defaultNoteLabels()
@@ -12,4 +17,5 @@ public class NotationalTonicContext: NotationalContext, @unchecked Sendable {
         (self.noteLabels[.tonicPicker]?[.mode] ?? false) ||
         (self.noteLabels[.tonicPicker]?[.guide] ?? false)
     }
+        
 }
