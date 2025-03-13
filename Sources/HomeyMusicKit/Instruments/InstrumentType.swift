6935b30902d1256895cc856822e63fac28d6319a
Brian Mulloy <brian@landlessness.net>
2025-03-13 14:26:49 -0400

    aded stub for tonnetz

--- Sources/HomeyMusicKit/Instruments/InstrumentType.swift
+++ Sources/HomeyMusicKit/Instruments/InstrumentType.swift
@@ -3,13 +3,14 @@ import MIDIKitIO
 
 public enum InstrumentType: MIDIChannel, CaseIterable, Identifiable, Codable {
     case isomorphic  = 0
-    case diamanti    = 1
-    case piano       = 2
-    case violin      = 3
-    case cello       = 4
-    case bass        = 5
-    case banjo       = 6
-    case guitar      = 7
+    case tonnetz     = 1
+    case diamanti    = 2
+    case piano       = 3
+    case violin      = 4
+    case cello       = 5
+    case bass        = 6
+    case banjo       = 7
+    case guitar      = 8
     case tonicPicker = 15
 
     public var id: Self { self }
@@ -21,6 +22,7 @@ public enum InstrumentType: MIDIChannel, CaseIterable, Identifiable, Codable {
     public var icon: String {
         switch self {
         case .isomorphic:  return "rectangle.split.3x1"
+        case .tonnetz:     return "circle.hexagongrid"
         case .diamanti:    return "diamond"
         case .piano:       return "pianokeys"
         case .violin:      return "guitars"
@@ -40,7 +42,7 @@ public extension InstrumentType {
     }
 
     static var keyboardInstruments: [InstrumentType] {
-        [.isomorphic, .diamanti, .piano]
+        [.isomorphic, .tonnetz, .diamanti, .piano]
     }
     
     var isKeyboardInstrument: Bool {
