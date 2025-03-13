319dae117461c4a9f9d47624dcdfa9b13309b89b
Brian Mulloy <brian@landlessness.net>
2025-03-13 18:07:26 -0400

    moving big chunks to homey music kit from homey pad

---
+++ Sources/HomeyMusicKit/Instruments/KeyboardInstruments/PitchDirectionPickerView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct PitchDirectionPickerView: View {
+    @EnvironmentObject var tonalContext: TonalContext
+
+    var body: some View {
+        HStack {
+            Picker("", selection: tonalContext.pitchDirectionBinding) {
+                Image(systemName: PitchDirection.downward.icon)
+                    .tag(PitchDirection.downward)
+                Image(systemName: PitchDirection.upward.icon)
+                    .tag(PitchDirection.upward)
+            }
+            .frame(maxWidth: 90)
+            .pickerStyle(.segmented)
+        }
+    }
+}
