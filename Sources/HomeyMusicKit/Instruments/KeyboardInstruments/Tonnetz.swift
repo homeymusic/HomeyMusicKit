b73ffb54da1c80637206d52a632aa229e846dd53
Brian Mulloy <brian@landlessness.net>
2025-03-16 13:09:34 -0400

    tonnetz in a grid not a hexagon yet

--- Sources/HomeyMusicKit/Instruments/KeyboardInstruments/Tonnetz.swift
+++ Sources/HomeyMusicKit/Instruments/KeyboardInstruments/Tonnetz.swift
@@ -7,7 +7,7 @@ public class Tonnetz: KeyboardInstrument {
         switch HomeyMusicKit.formFactor {
         case .iPhone:
             super.init(instrumentType: .tonnetz,
-                       defaultRows: 0, minRows: 0, maxRows: 5,
+                       defaultRows: 1, minRows: 0, maxRows: 5,
                        defaultCols: 8, minCols: 6, maxCols: 18)
         case .iPad:
             super.init(instrumentType: .tonnetz,
--- Sources/HomeyMusicKit/Instruments/KeyboardInstruments/TonnetzView.swift
+++ Sources/HomeyMusicKit/Instruments/KeyboardInstruments/TonnetzView.swift
@@ -3,19 +3,20 @@ import MIDIKitCore
 
 struct TonnetzView: View {
     @ObservedObject var tonnetz: Tonnetz
-
+    
     @EnvironmentObject var tonalContext: TonalContext
-
+    
     var body: some View {
         VStack(spacing: 0) {
             ForEach(tonnetz.rowIndices, id: \.self) { row in
                 HStack(spacing: 0) {
                     ForEach(tonnetz.colIndices(forTonic: Int(tonalContext.tonicPitch.midiNote.number),
-                                                          pitchDirection: tonalContext.pitchDirection), id: \.self) { col in
-                        let linearIndex: Int = Int(col) + 12 * Int(row)
+                                               pitchDirection: tonalContext.pitchDirection), id: \.self) { col in
+                        let pitchMIDI: Int = (7*Int(col) + -4 * Int(row))
+                        let pitchClassMIDI: Int = pitchMIDI % 12 + Int(tonalContext.tonicPitch.midiNote.number)
                         Group {
-                            if Pitch.isValid(linearIndex) {
-                                let pitch = tonalContext.pitch(for: MIDINoteNumber(linearIndex))
+                            if Pitch.isValid(pitchClassMIDI) {
+                                let pitch = tonalContext.pitch(for: MIDINoteNumber(pitchClassMIDI))
                                 PitchContainerView(
                                     pitch: pitch
                                 )
