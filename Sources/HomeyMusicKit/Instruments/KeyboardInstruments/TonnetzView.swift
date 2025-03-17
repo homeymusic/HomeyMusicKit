74ac485923df72c0cf5eaf095573da961f0c0b6e
Brian Mulloy <brian@landlessness.net>
2025-03-17 16:23:39 -0400

    layout finnally working for tonnetz

--- Sources/HomeyMusicKit/Instruments/KeyboardInstruments/TonnetzView.swift
+++ Sources/HomeyMusicKit/Instruments/KeyboardInstruments/TonnetzView.swift
@@ -9,28 +9,28 @@ struct TonnetzView: View {
         GeometryReader { geometry in
             let rowIndices = tonnetz.rowIndices
             let colIndices = tonnetz.colIndices
-            let cellWidth = geometry.size.width / CGFloat(colIndices.count)
-            let cellHeight = geometry.size.height / CGFloat(rowIndices.count)
+            let cellWidth  = geometry.size.width / CGFloat(colIndices.count)
             
             VStack(spacing: 0) {
                 ForEach(rowIndices, id: \.self) { row in
+                    let integerOffset = Int(floor(Double(row) / 2.0))
+                    let fractionalOffset = Double(row) / 2.0 - Double(integerOffset)
                     HStack(spacing: 0) {
-                        ForEach(colIndices, id: \.self) { col in
-                            let pitchMIDI: Int = (7 * Int(col)) + (4 * Int(row))
+                        ForEach(colIndices.indices, id: \.self) { index in
+                            let col = colIndices[index]
+                            let isLastCol = (index == colIndices.count - 1)
+                            let pitchMIDI: Int = (7 * (Int(col) - integerOffset)) + (4 * Int(row))
                             let pitchClassMIDI: Int = modulo(pitchMIDI, 12) + Int(tonalContext.tonicPitch.midiNote.number)
                             Group {
-                                if Pitch.isValid(pitchClassMIDI) {
+                                if Pitch.isValid(pitchClassMIDI) && !(isLastCol && fractionalOffset != 0.0) {
                                     let pitch = tonalContext.pitch(for: MIDINoteNumber(pitchClassMIDI))
                                     PitchContainerView(pitch: pitch)
-                                        .frame(width: cellWidth, height: cellHeight)
                                 } else {
                                     Color.clear
-                                        .frame(width: cellWidth, height: cellHeight)
                                 }
                             }
-                        }
-                    }
-                    .offset(x: CGFloat(row) * (cellWidth * 0.5))
+                        }                    }
+                    .offset(x: fractionalOffset * cellWidth)
                 }
             }
             .frame(width: geometry.size.width, height: geometry.size.height)
