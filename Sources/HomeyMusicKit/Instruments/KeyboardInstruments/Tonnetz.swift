d64fb624c78c2c10de0d34387eb13aaf1f13d6a8
Brian Mulloy <brian@landlessness.net>
2025-03-17 19:00:39 -0400

    same dimensions for iphone and ipad for tonnetz

--- Sources/HomeyMusicKit/Instruments/KeyboardInstruments/Tonnetz.swift
+++ Sources/HomeyMusicKit/Instruments/KeyboardInstruments/Tonnetz.swift
@@ -2,22 +2,14 @@ import Foundation
 import MIDIKitIO
 
 public class Tonnetz: KeyboardInstrument {
-    @MainActor
     public init() {
-        switch HomeyMusicKit.formFactor {
-        case .iPhone:
-            super.init(instrumentChoice: .tonnetz,
-                       defaultRows: 2, minRows: 1, maxRows: 5,
-                       defaultCols: 4, minCols: 1, maxCols: 18)
-        case .iPad:
-            super.init(instrumentChoice: .tonnetz,
-                       defaultRows: 0, minRows: 0, maxRows: 2,
-                       defaultCols: 13, minCols: 6, maxCols: 18)
-        }
+        super.init(instrumentChoice: .tonnetz,
+                   defaultRows: 1, minRows: 1, maxRows: 18,
+                   defaultCols: 3, minCols: 1, maxCols: 18)
     }
     
     public var colIndices: [Int] {
         Array(-cols ... cols)
     }
-
+    
 }
