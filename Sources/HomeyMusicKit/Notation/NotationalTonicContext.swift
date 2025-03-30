09f5203d9e673243eab0899075e347cc6bec8883
Brian Mulloy <brian@landlessness.net>
2025-03-30 09:13:25 -0400

    home and help toggles work now

--- Sources/HomeyMusicKit/Notation/NotationalTonicContext.swift
+++ Sources/HomeyMusicKit/Notation/NotationalTonicContext.swift
@@ -1,14 +1,26 @@
 import SwiftUI
 
+@Observable
 public class NotationalTonicContext: NotationalContext {
     
     // Persisted properties specific to the tonic context.
     @ObservationIgnored
-    @AppStorage("showHelp") public var showHelp: Bool = false
+    @AppStorage("showHelp") public var showHelpRaw: Bool = false
+    
+    public var showHelp: Bool = false {
+        didSet {
+            showHelpRaw = showHelp
+        }
+    }
     
     @ObservationIgnored
-    @AppStorage("showTonicPicker") public var showTonicPicker: Bool = false
-    
+    @AppStorage("showTonicPicker") public var showTonicPickerRaw: Bool = false
+    public var showTonicPicker: Bool = false {
+        didSet {
+            showTonicPickerRaw = showTonicPicker
+        }
+    }
+
     // Override the key prefix so that all persisted keys are namespaced.
     public override var keyPrefix: String { "tonic" }
     
@@ -24,6 +36,12 @@ public class NotationalTonicContext: NotationalContext {
     public var showModes: Bool {
         (self.noteLabels[.tonicPicker]?[.mode] ?? false) ||
         (self.noteLabels[.tonicPicker]?[.guide] ?? false)
+    }
+    
+    override public init() {
+        super.init()
+        self.showHelp = showHelpRaw
+        self.showTonicPicker = showTonicPickerRaw
     }
     
 }
