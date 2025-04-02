908fd51e23c03ffab6bddd1321f901c3166aee86
Brian Mulloy <brian@landlessness.net>
2025-04-02 16:48:16 -0400

    comment cleanup;

--- Sources/HomeyMusicKit/Instruments/Instrument.swift
+++ Sources/HomeyMusicKit/Instruments/Instrument.swift
@@ -7,12 +7,4 @@ public class Instrument: ObservableObject {
     public init(instrumentChoice: InstrumentChoice) {
         self.instrumentChoice = instrumentChoice
     }
-    
-    // TODO: put all the notational settings in here?
-    // current thinking? No. In fact, maybe it is better
-    // if (like accidental choice) any chaoice about metadata
-    // applies to all the instrumnts.
-    // then create an InstrumentsDefaultsManager for it?
-    // and move it all into HomeyMusicKit?
-    // and accidental setting probably goes in here?
 }
