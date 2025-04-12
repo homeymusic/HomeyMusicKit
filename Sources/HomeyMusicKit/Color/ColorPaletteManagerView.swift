3251356b0a4a466dc7f1441a76809db22d5e2659
Brian Mulloy <brian@landlessness.net>
2025-04-12 19:36:20 -0400

    dropped nav view for a custom look for palette manager

--- Sources/HomeyMusicKit/Color/ColorPaletteManagerView.swift
+++ Sources/HomeyMusicKit/Color/ColorPaletteManagerView.swift
@@ -5,26 +5,37 @@ struct ColorPaletteManagerView: View {
     @Environment(\.dismiss) private var dismiss
     
     var body: some View {
-        NavigationStack {
+        VStack {
+            ZStack {
+                HStack {
+                    Spacer()
+                    Image(systemName: "swatchpalette")
+                        .background(
+                            RoundedRectangle(cornerRadius: 3)
+                                .foregroundColor(.systemGray6)
+                        )
+                    Spacer()
+                }
+                HStack {
+                    Spacer()
+                    Button("Done") {
+                        dismiss()
+                    }
+                    .background(
+                        RoundedRectangle(cornerRadius: 3)
+                            .foregroundColor(.systemGray6)
+                    )
+                }
+            }
+            .padding(.top, 11)
             HStack(spacing: 0) {
                 ColorPaletteListView()
                 ColorPaletteEditorView()
                 ColorPalettePreviewView()
             }
-            .toolbar {
-                ToolbarItem(placement: .principal) {
-                    HStack {
-                        Image(systemName: "swatchpalette")
-                    }
-                }
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button("Done") {
-                        dismiss()
-                    }
-                }
-            }
-            .environment(\.editMode, .constant(.active))
         }
+        .environment(\.editMode, .constant(.active))
+        .navigationBarHidden(true)
     }
 }
 
